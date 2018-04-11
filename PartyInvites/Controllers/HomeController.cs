98ab6961a15480d91ebaeb6c7abe88c72da751ba
Sean Newman <seangnewman@msn.com>
2018-04-10 20:42:37 -0700

    Completed PartyInvite Example

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -6,32 +6,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 
+
+
 namespace PartyInvites.Controllers
 {
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+         public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning!" : "Good Afternoon!";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            //ToDo: store response from Guest
+            if (ModelState.IsValid)  // Passes all constraints
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //A validation error occurred
+                return View();
+            }
+            
         }
 
-        public IActionResult Contact()
+        public ViewResult ListResponses()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
